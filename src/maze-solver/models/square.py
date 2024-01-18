0fa8635c3c27d7f5f794108eb49ce1159fa15b4e
rsy <intriqet@gmail.com>
2024-01-18 03:22:03 -0800

    added mazes, src, and pre-commit

---
+++ src/maze-solver/models/square.py
@@ -0,0 +1,19 @@
+# model/square.py
+
+# Convey information about maze lcation
+# Every square has a coordinate
+# Squares should have border pattern. Roles optional
+
+from dataclasses import dataclass
+
+from maze_solver.models.border import Border
+from maze_solver.models.role import Role
+
+
+@dataclass(frozen=True)
+class Square:
+    index: int
+    row: int
+    column: int
+    border: Border
+    role: Role = Role.NONE
